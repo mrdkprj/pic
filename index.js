8089197fc8f3996cdc3d3cc0865854b34704057d
unknown <longsight.d.m@gmail.com>
2021-09-29 19:44:27 +0900

    zoom

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ window.onload = function(){
     document.addEventListener("keydown", (e) => {
 
         if(e.ctrlKey && e.key == "q"){
-            rotate();
+            //rotate();
+            max();
         }
 
         if(e.key === "Delete"){
@@ -80,10 +81,183 @@ window.onload = function(){
         if(e.dataTransfer.items[0].kind === "file" && e.dataTransfer.items[0].type.includes("image")){
             dropFile(e.dataTransfer.items[0].getAsFile());
         }
+    });
+
+    let moving = false;
+    document.getElementById("img").addEventListener("mousedown", e => {
+        moving = true;
+        setPos(e);
+    })
+
+    document.addEventListener("mousemove", e => {
+        if(moving){
+            e.preventDefault();
+            moveImage(e);
+        }
+    })
+
+    document.addEventListener("mouseup", e => {
+        moving = false;
+    })
+
+    document.getElementById("img").addEventListener("wheel", e => {
+        zoom(e);
     })
 
     init();
 
+}
+
+let x;
+let y;
+let containerRect;
+let originalImgRect = {top:0, left:0, width:0, height:0};
+let imgRect = {top:0, left:0, width:0, height:0};
+let scale = 1;
+let minScale;
+let zoomed = false;
+
+function zoom(event) {
+
+    let scaleDirection;
+
+    if(!zoomed){
+        initScale();
+    }
+
+    event.preventDefault();
+
+    scale += event.deltaY * -0.001;
+
+    // Restrict scale
+    if(event.deltaY < 0){
+        scale = Math.min(Math.max(.125, scale), 4);
+        scaleDirection = 1;
+    }else{
+        scale = Math.max(Math.max(.125, scale), minScale);
+        scaleDirection = 0;
+    }
+
+    // Apply scale transform
+    const img = document.getElementById("img");
+    img.style.transform = `scale(${scale})`;
+
+    onScaleChanged(scaleDirection);
+
+  }
+
+function initScale(){
+    const area = document.getElementById("imageArea");
+
+    area.classList.add("org");
+
+}
+
+function resetScale(){
+    const area = document.getElementById("imageArea");
+
+    if(area.classList.contains("org")){
+        //area.classList.remove("org");
+    }
+
+    const img = document.getElementById("img");
+    img.style.top = null;
+    img.style.left = null;
+    scale = minScale;
+    zoomed = false;
+    img.style.transform = `scale(${scale})`;
+}
+
+function onScaleChanged(scaleDirection){
+    const img = document.getElementById("img");
+    let rect = img.getBoundingClientRect();
+    createRect(imgRect, rect);
+
+    console.log(scale > minScale)
+    if(scale > minScale){
+        console.log(imgRect)
+        console.log(originalImgRect)
+        imgRect.top = (imgRect.height - originalImgRect.height) / 2;
+        console.log(imgRect.top)
+    }
+
+        if(img.offsetTop > imgRect.top ){
+            img.style.top = imgRect.top + "px";
+        }
+
+        if(img.offsetTop < imgRect.top * -1){
+
+            img.style.top = imgRect.top * -1 + "px";
+        }
+
+
+
+
+}
+
+function setPos(e){
+    x = e.x;
+    y = e.y;
+
+    containerRect = document.getElementById("imageArea").getBoundingClientRect();
+}
+
+let t = 0;
+function moveImage(e){
+    const img = document.getElementById("img");
+    const cont = document.getElementById("imageArea")
+
+    let right = false;
+    const dx = e.x - x;
+    if(dx > 0){
+        right = true;
+    }
+    x = e.x;
+
+    let down = false;
+    const dy = e.y - y;
+    if(dy > 0){
+        down = true;
+    }
+    y = e.y;
+
+    if(t > 10){
+        //return;
+    }
+
+
+    if(down){
+        if(img.offsetTop + dy > imgRect.top){
+
+        }else{
+            img.style.top = (img.offsetTop + dy) + "px"
+        }
+    }
+
+    if(!down){
+        if(img.offsetTop + dy < imgRect.top * -1){
+
+        }else{
+            img.style.top = (img.offsetTop - Math.abs(dy)) + "px"
+        }
+    }
+
+    if(right){
+        if(img.offsetLeft + dx > containerRect.left){
+
+        }else{
+            img.style.left = (img.offsetLeft + dx) + "px"
+        }
+    }
+
+    if(!right){
+        if(img.offsetLeft + imgRect.width + dx < containerRect.width){
+
+        }else{
+            img.style.left = (img.offsetLeft - Math.abs(dx)) + "px"
+        }
+    }
+t++
 }
 
 function rotate(){
@@ -130,14 +304,27 @@ function deleteFile(){
     }
 }
 
+function createRect(target, base){
+    target.top = base.top;
+    target.left = base.left;
+    target.width = base.width;
+    target.height = base.height;
+}
+
 window.api.receive("afterfetch", data => {
     if(data){
+        resetScale();
         const img = document.getElementById("img");
         img.src = data.path;
         const dummy = new Image();
         dummy.src = data.path;
         dummy.onload = function(){
             document.title = "PicViewer - " + data.name + "(" + dummy.width + " x " + dummy.height + ")";
+            createRect(originalImgRect, img.getBoundingClientRect());
+            originalImgRect.top = img.offsetTop;
+            originalImgRect.left = img.offsetLeft;
+            scale = 1//Math.min(originalImgRect.width / dummy.width, originalImgRect.height / dummy.height, 1);
+            minScale = scale;
             document.getElementById("counter").textContent = data.counter;
             document.getElementById("loader").style.display = "none";
         }
